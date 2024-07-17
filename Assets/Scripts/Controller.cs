ebecac4e970edcd04882113be1acabadb9ff2c75
Max Zorzetti <max.zorzetti@wildlifestudios.com>
2024-07-17 10:59:44 -0300

    Add Todos

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,10 +1,17 @@
 using UnityEngine;
 
+// TODO: Look into Unity's Input System
+// https://docs.unity3d.com/ScriptReference/Input.html
+// You can define axis/buttons in the Input Manager
+// https://docs.unity3d.com/Manual/class-InputManager.html
 public class Controller {
+    // TODO: Use Input.GetAxis for movement
     public ControllerKey Up =  new(KeyCode.W);
     public ControllerKey Left =  new(KeyCode.A);
     public ControllerKey Down =  new(KeyCode.S);
     public ControllerKey Right =  new(KeyCode.D);
+    // TODO: Use Input.GetButtonDown for action
+    // Input.GetButton_Down_ returns true only in the _first frame_ it is pressed
     public ControllerKey MainAction = new(KeyCode.Space);
 
     public bool IsPressingMovementKeys() {
