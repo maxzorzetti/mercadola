fb633f8187c8eaa387df314c5c02c08b45430716
Max Zorzetti <max.zorzetti@wildlifestudios.com>
2024-07-18 13:51:46 -0300

    Add collection animation

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,11 +1,30 @@
+using System;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
     public Event CollectEvent;
 
+    [Range(0f, 2f)]
+    public float CollectionTime = 0.25f;
+
+    public Transform ShrinkTarget;
+    
+    float collectionSpeed => 1 / CollectionTime;
+    
+    
+    public AnimationCurve CollectionCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    
+    bool isCollected;
+    Progression collectProgression;
+    SpriteRenderer spriteRenderer;
+    
     void Start()
     {
+        // Get a sprite to shrink if none was set
+        ShrinkTarget ??= GetComponentInChildren<SpriteRenderer>().transform; 
+        collectProgression = new Progression();
+        
         if (CollectEvent == null)
         {
             Debug.LogWarning($"Object {name} is missing an OnCollectEvent!");
@@ -14,7 +33,25 @@ public class Collectible : MonoBehaviour
     
     public void Collect()
     {
+        if (isCollected) return;
+        isCollected = true;
         CollectEvent.Raise();
-        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (isCollected) DoCollection();
+    }
+
+    void DoCollection()
+    {
+        CollectionCurve.Evaluate(collectProgression.ProgressRate);
+        ShrinkTarget.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, collectProgression.ProgressRate);
+        
+        collectProgression.Advance(Time.deltaTime * collectionSpeed);
+        if (collectProgression.Consume())
+        {
+            Destroy(gameObject);
+        }
     }
 }
