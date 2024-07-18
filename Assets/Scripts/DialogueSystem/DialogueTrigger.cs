825f58a9374a67b6d12c4c139a8908853093980d
Max Zorzetti <max.zorzetti@wildlifestudios.com>
2024-07-18 13:19:20 -0300

    Add possible Event to DialogueTrigger

--- Assets/Scripts/DialogueSystem/DialogueTrigger.cs
+++ Assets/Scripts/DialogueSystem/DialogueTrigger.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour {
-
+	
+	public Event DialogueEvent;
+	
 	public Dialogue dialogue;
 
 	public void TriggerDialogue ()
 	{
 		FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+		if (DialogueEvent != null) DialogueEvent.Raise();
 	}
 
 }
